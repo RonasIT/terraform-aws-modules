fd8410ac1fcb7936564176af5160874434684804
Aleksei Kiyashkin <akiyashkin@ronasit.com>
2024-05-24 13:55:59 +0000

    parametrize everything in k8s-addons

--- modules/k8s-addons/main.tf
+++ modules/k8s-addons/main.tf
@@ -5,16 +5,17 @@ resource "helm_release" "ingress_nginx" {
   repository       = "https://kubernetes.github.io/ingress-nginx"
   chart            = "ingress-nginx"
   namespace        = "ingress-nginx"
-  version          = "4.10.0"
+  version          = var.ingress_nginx_chart_version
   create_namespace = true
 
-  set {
-    name  = "controller.allowSnippetAnnotations"
-    value = "true"
+  dynamic "set" {
+    for_each = var.ingress_nginx_set_values
+    content {
+      name  = set.key
+      value = set.value
+    }
   }
 }
-
-
 
 resource "helm_release" "cert_manager" {
   count = var.cert_manager_enabled ? 1 : 0
@@ -23,11 +24,33 @@ resource "helm_release" "cert_manager" {
   repository       = "https://charts.jetstack.io"
   chart            = "cert-manager"
   namespace        = "cert-manager"
-  version          = "v1.14.3"
+  version          = var.cert_manager_chart_version
   create_namespace = true
 
-  set {
-    name  = "installCRDs"
-    value = "true"
+  dynamic "set" {
+    for_each = var.cert_manager_set_values
+    content {
+      name  = set.key
+      value = set.value
+    }
+  }
+}
+
+resource "helm_release" "metrics_server" {
+  count = var.metrics_server_enabled ? 1 : 0
+
+  name             = "metrics-server"
+  repository       = "https://kubernetes-sigs.github.io/metrics-server/"
+  chart            = "metrics-server"
+  namespace        = "kube-system"
+  version          = var.metrics_server_chart_version
+  create_namespace = false
+
+  dynamic "set" {
+    for_each = var.metrics_server_set_values
+    content {
+      name  = set.key
+      value = set.value
+    }
   }
 }
--- modules/k8s-addons/variables.tf
+++ modules/k8s-addons/variables.tf
@@ -4,8 +4,62 @@ variable "cert_manager_enabled" {
   description = "Deploy cert-manager into cluster (https://cert-manager.io/)"
 }
 
+variable "cert_manager_chart_version" {
+  type        = string
+  default     = "v1.14.3"
+  description = "Cert Manager Helm chart version"
+}
+
+variable "cert_manager_set_values" {
+  type = map(string)
+  default = {
+    "installCRDs" = "true"
+  }
+  description = "Set values for cert-manager Helm chart"
+}
+
 variable "ingress_nginx_enabled" {
   type        = bool
   default     = true
   description = "Deploy ingress-nginx into cluster (https://kubernetes.github.io/ingress-nginx/)"
+}
+
+variable "ingress_nginx_chart_version" {
+  type        = string
+  default     = "4.10.0"
+  description = "Ingress NGINX Helm chart version"
+}
+
+variable "ingress_nginx_set_values" {
+  type = map(string)
+  default = {
+    "controller.allowSnippetAnnotations" = "true",
+    "controller.proxy-body-size"         = "100m"
+  }
+  description = "Set values for ingress-nginx Helm chart"
+}
+
+
+variable "metrics_server_enabled" {
+  type        = bool
+  default     = true
+  description = "Deploy metrics-server into cluster (https://github.com/kubernetes-sigs/metrics-server)"
+}
+
+variable "metrics_server_application_version" {
+  type        = string
+  default     = "0.7.1"
+  description = "Metrics Server application version"
+}
+
+variable "metrics_server_chart_version" {
+  type        = string
+  default     = "3.12.1"
+  description = "Metrics Server Helm chart version"
+}
+
+variable "metrics_server_set_values" {
+  type        = map(string)
+  default     = {}
+  description = "Set values for metrics-server Helm chart"
 }
